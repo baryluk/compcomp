db5ca510ee15ad74f06677fa316bceae593d68e0
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2022-02-27 18:40:01 +0100

    Improvements, tabulate, progress, recursion, detect compressors, filter compressors, cp, fix lz4, round to 4k, etc.

--- README.md
+++ README.md
@@ -4,3 +4,92 @@ compcomp - compression comparator
 `./compcomp.py` takes as input list of files, and test various
 compression algorithms on them. For each file it will compute the time,
 compression speed and compression ratio.
+
+Can test individual files or directories recursively:
+
+```
+$ ./compcomp.py --methods="gzip-6|lz4-9|zstd.*|xz.*" --progress .
+Methods: gzip-6 xz-1 xz-3 xz-6 xz-9 zstd-1 zstd-3 zstd-14 zstd-16 zstd-19 lz4-9
+. 53 files 66506 bytes
+100%|████████████████████████████████████████████████| 66.5k/66.5k [00:01<00:00, 34.2kB/s]
+| comp    |   count |   in_size |   out_size |   ratio |       min-max |   in_size4k |   out_size4k |   ratio |       min-max |          speed |
+|:--------|--------:|----------:|-----------:|--------:|--------------:|------------:|-------------:|--------:|--------------:|---------------:|
+| gzip-6  |      53 |     66506 |      38634 |  58.09% | 18.8 - 273.9% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.671033 MiB/s |
+| xz-1    |      53 |     66506 |      40984 |  61.62% | 21.3 - 347.8% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.302411 MiB/s |
+| xz-3    |      53 |     66506 |      40912 |  61.52% | 21.3 - 347.8% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.245949 MiB/s |
+| xz-6    |      53 |     66506 |      40360 |  60.69% | 21.3 - 347.8% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.219425 MiB/s |
+| xz-9    |      53 |     66506 |      40360 |  60.69% | 21.3 - 347.8% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.198036 MiB/s |
+| zstd-1  |      53 |     66506 |      38475 |  57.85% | 16.6 - 156.5% |      241664 |       225280 |  93.22% | 50.0 - 100.0% | 0.532512 MiB/s |
+| zstd-3  |      53 |     66506 |      38123 |  57.32% | 16.6 - 156.5% |      241664 |       225280 |  93.22% | 50.0 - 100.0% | 0.579296 MiB/s |
+| zstd-14 |      53 |     66506 |      37276 |  56.05% | 16.6 - 156.5% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.548926 MiB/s |
+| zstd-16 |      53 |     66506 |      37233 |  55.98% | 16.6 - 156.5% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.514238 MiB/s |
+| zstd-19 |      53 |     66506 |      37219 |  55.96% | 16.6 - 156.5% |      241664 |       221184 |  91.53% | 25.0 - 100.0% | 0.500920 MiB/s |
+| lz4-9   |      53 |     66506 |      44921 |  67.54% | 17.6 - 182.6% |      241664 |       225280 |  93.22% | 50.0 - 100.0% | 0.641763 MiB/s |
+```
+
+Columns with "4k" suffix, do have all input and output byte sizes of each
+file rounded up to 4KiB first, this is useful, for example for estimating
+compression ration of small files on file systems with built-in
+compression, like zfs, btrfs, or squashfs.
+
+```
+$ ./compcomp.py --list
+cp
+gzip-6
+gzip-9
+zopfli-i15
+zopfli-i50
+bz2-4
+bz2-9
+xz-6
+xz-9
+lzma-6
+lzma-9
+zstd-3
+zstd-14
+zstd-16
+zstd-19
+lz4-1
+lz4-4
+lz4-9
+lz4-12
+lzop-1
+lzop-3
+lzop-7
+lzop-9
+br-3
+br-9
+br-best
+7z-ultra
+zip
+rar
+arj-m1
+arj-jm
+lrzip-1-bz2
+lrzip-9-bz2
+lrzip-1-lzo
+lrzip-9-lzo
+lrzip-1-zpaq
+lrzip-9-zpaq
+snappy-1
+snappy-6
+snappy-9
+dact
+```
+
+
+
+Pass `--progress` to show progress bar, useful when processing big directories.
+
+Pass `--verbose` to show compression stats for every file being processed
+in real time. Should not be combined with `--progress`.
+
+Pass `--debug` to show even more details.
+
+Files before being passed to compressed are copied to `/tmp` (or `TEMP`
+if configured), so the file is in cache, and prevent any risk of
+potentially corrupting data in passed original files or directories.
+
+Installation: Just run `compcomp.py` directly. `tabulate` and `tqdm`
+packages are recommended to enable nicer output and progress bar, but not
+required.
--- compcomp.py
+++ compcomp.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 
+import argparse
+import dataclasses
 import collections
 import sys
 import os
+import re
+import shutil
 import subprocess
 import time
-from typing import Any, Dict, List
+import tempfile
+from typing import Any, Dict, List, Optional, Tuple
+
+verbose = False
+debug = False
 
 comps = [
-    # ("cat",                   ".cat",    ["cat", "FILE"]),  # dry-run
-    ("wc",                    ".wc",     ["wc", "FILE"]),  # dry-run
-    *[(f"gzip-{level}",       ".gz",     ["gzip", "--keep", f"-{level}", "FILE"]) for level in [6, 9]],
+      ("cp",                  ".copy",   ["cp", "FILE", "FILE.copy"]),  # dry-run
+    *[(f"gzip-{level}",       ".gz",     ["gzip", "--keep", f"-{level}", "FILE"]) for level in [1, 3, 6, 9]],
     *[(f"zopfli-i{iters}",    ".gz",     ["zopfli", f"--i{iters}", "FILE"]) for iters in [15, 50]],
-    *[(f"bz2-{level}",        ".bz2",    ["bzip2", "--keep", f"-{level}", "FILE"]) for level in [4, 9]],
-    *[(f"xz-{level}",         ".xz",     ["xz", "--keep", f"-{level}", "FILE"]) for level in [6, 9]],
+    *[(f"bz2-{level}",        ".bz2",    ["bzip2", "--keep", f"-{level}", "FILE"]) for level in [4, 6, 9]],
+    *[(f"xz-{level}",         ".xz",     ["xz", "--keep", f"-{level}", "FILE"]) for level in [1, 3, 6, 9]],
     *[(f"lzma-{level}",       ".lzma",   ["lzma", "--keep", f"-{level}", "FILE"]) for level in [6, 9]],
-    *[(f"zstd-{level}",       ".zst",    ["zstd", "--quiet", "--keep", f"-{level}", "FILE"]) for level in [3, 14, 16]],
-    *[(f"lz4-{level}",        ".lz4",    ["lz4", "--quiet", "--keep", f"-{level}", "FILE"]) for level in [1, 4, 9, 12]],
+    *[(f"zstd-{level}",       ".zst",    ["zstd", "--quiet", "--keep", f"-{level}", "FILE"]) for level in [1, 3, 14, 16, 19]],
+    # See https://bugs.debian.org/998207
+    *[(f"lz4-{level}",        ".lz4",    ["lz4", "--quiet", "--keep", f"-{level}", "FILE", "FILE.lz4"]) for level in [1, 4, 9, 12]],
     *[(f"lzop-{level}",       ".lzo",    ["lzop", "--keep", f"-{level}", "FILE"]) for level in [1, 3, 7, 9]],
-    *[(f"br-{level}",         ".br",     ["brotli", "--keep", f"-{level}", "FILE"]) for level in [3, 9]],
+    *[(f"br-{level}",         ".br",     ["brotli", "--keep", f"-{level}", "FILE"]) for level in [1, 3, 9]],
       ("br-best",             ".br",     ["brotli", "--keep", "--best", "FILE"]),
       ("7z-ultra",            ".7z",     ["7z", "a", "-bd", "-bb0", "-bso0", "-t7z", "-m0=lzma", "-mx=9", "-mfb=64", "-md=32m", "-ms=on", "BASE.7z", "FILE_OR_DIR"]),
       ("zip",                 ".zip",    ["zip", "--quiet", "BASE.zip", "FILE"]),   # To add directory, enable recurse mode (-r).
@@ -33,9 +41,9 @@ comps = [
     *[(f"lrzip-{level}-lzo",  ".lrz",    ["lrzip", "--quiet", "-p", "1", "-L", f"{level}", "-N", "0", "--lzo", "FILE"]) for level in [1, 9]],
     *[(f"lrzip-{level}-zpaq", ".lrz",    ["lrzip", "--quiet", "-p", "1", "-L", f"{level}", "-N", "0", "--zpaq", "FILE"]) for level in [1, 9]],
     # https://github.com/kubo/snzip
-    *[(f"snappy-{level}",     ".sz", ["snzip", "-k", "FILE"]) for level in [1, 6, 9]],
+    *[(f"snappy-{level}",     ".sz",     ["snzip", "-k", "FILE"]) for level in [1, 6, 9]],
     # https://github.com/odeke-em/snappy-cli/tree/master/cmd/snappy-compress
-    # *[(f"snappy-{level}-go", ".snappy", ["snappy-compress", "FILE", "STDOUT"]) for level in [1, 6, 9]],
+    # *[(f"snappy-{level}-go", ".snappy",  ["snappy-compress", "FILE", "STDOUT"]) for level in [1, 6, 9]],
       ("dact",                ".dct",    ["dact", "FILE"]),
 
     # zpaq
@@ -47,65 +55,260 @@ comps = [
     # arc
 ]
 
-def test_file(filename: str) -> Dict[str, Any]:
+
+def find_exeuctable(x: str) -> Optional[str]:
+    if not x:
+        return None
+    if x[0] == "/" or x.startswith("./"):
+        return x if os.path.isfile(x) else None
+    for path in os.environ["PATH"].split(":"):
+        full_path = os.path.join(path, x)
+        if os.path.isfile(full_path):
+             if debug:
+                 print(f"Found {x} at {full_path}", file=sys.stderr)
+             return full_path
+
+    if debug:
+        print(f"Did not found {x} in PATH", file=sys.stderr)
+    return None
+
+
+def run_one(filename: str, comp: Tuple[str, str, str]) -> Optional[str]:
+    key, suffix, command = comp
+
+    command = [arg.replace("FILE_OR_DIR", filename).replace("FILE", filename) for arg in command]
+    assert suffix
+    assert suffix.startswith(".")
+    output = f"{filename}{suffix}"
+    if any("BASE" in arg for arg in command):
+        output = f"{filename}_output_{key}{suffix}"
+        command = [arg.replace("BASE", f"{filename}_output_{key}") for arg in command]
+    try:
+        os.unlink(output)
+    except FileNotFoundError:
+        pass
+
+    try:
+        if debug:
+            print("Running:", command, file=sys.stderr)
+        if debug:
+            completed_process = subprocess.run(command)
+        else:
+            completed_process = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except FileNotFoundError as e:
+        # print("error", e)
+        # Program not installed.
+        return None
+
+    return output
+
+
+def test_file(original_filename: str) -> Dict[str, Any]:
     results = {}
-    input_size = os.stat(filename).st_size
-    for key, suffix, command in comps:
+    input_size = os.stat(original_filename).st_size
+    # Add custom suffix, so we do not accidently use "_z" which gzip does not like.
+    with tempfile.NamedTemporaryFile(suffix="compcomp") as tmp:
+        filename = tmp.name
+        shutil.copyfile(original_filename, filename)
+        for key, suffix, command in comps:
+            input_size = os.stat(filename).st_size
 
-        input_size = os.stat(filename).st_size
+            if verbose:
+                print(f"{key:20s}", original_filename, end=" ")
 
-        command = [arg.replace("FILE_OR_DIR", filename).replace("FILE", filename) for arg in command]
-        assert suffix
-        assert suffix.startswith(".")
-        output = f"{filename}{suffix}"
-        if any("BASE" in arg for arg in command):
-            output = f"{filename}_output_{key}{suffix}"
-            command = [arg.replace("BASE", f"{filename}_output_{key}") for arg in command]
-        try:
-            os.unlink(output)
-        except FileNotFoundError:
-            pass
-        t0 = time.monotonic()
-        print(f"{key:20s}", filename, end=" ")
-        try:
-            completed_process = subprocess.run(command) #, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        except FileNotFoundError as e:
-            print("error", e)
-            # Program not installed.
-            continue
-        t1 = time.monotonic()
-        output_size = None
-        try:
-            output_size = os.stat(output).st_size
-        except FileNotFoundError as e:
-            print("error", e)
-            continue
-        print(f"{t1 - t0:.3f}s", output_size, f"{input_size / (t1 - t0) / 1e6:.2f}MB/s")
+            t0 = time.monotonic()
+            output = run_one(filename, (key, suffix, command))
+            t1 = time.monotonic()
 
-        try:
-            os.unlink(output)
-        except FileNotFoundError:
-            print(f"{key}: Cannot unlink output {output}", file=sys.stderr)
-            continue
-        results[key] = (t1 - t0, input_size, output_size)
-    return results
+            if output is None:
+                continue
 
-def main() -> None:
-    all_stats = collections.defaultdict(list)
+            try:
+                output_size = os.stat(output).st_size
+            except FileNotFoundError as e:
+                print("error", e)
+                continue
 
-    for filename in sys.argv[1:]:
+            if verbose:
+                print(f"{t1 - t0:.3f}s", output_size, f"{input_size / (t1 - t0) / 1e6:.2f}MB/s")
+
+            try:
+                os.unlink(output)
+            except FileNotFoundError:
+                print(f"{key}: Cannot unlink output {output}", file=sys.stderr)
+                continue
+            results[key] = (t1 - t0, input_size, output_size)
+        return results
+
+def process_iterable(filenames):
+    for filename in filenames:
         stats_for_file = test_file(filename)
         for key, stat in stats_for_file.items():
-            all_stats[key].append(stat)
+            yield key, stat
 
-    print()
-    print("Stats:")
-    longest_key = max(len(key) for key in all_stats.keys())
-    for key, stats in all_stats.items():
-        print(f"{key:{longest_key}s}", end="")
-        for stat in stats:
-            print(f" {stat[2] / max(1, stat[1]):.3f} {stat[0]:8.3f}s {max(1, stat[1]) / max(0.001, stat[0]) / 1e6:.2f}MB/s", end="")
+def roundup(x, b = 4096):
+    return (x + b - 1) // b * b
+
+@dataclasses.dataclass
+class Stats:
+    total_files_count: int = 0
+    total_input_size: int = 0
+    total_input_size_4k: int = 0
+    total_compressed_size: int = 0
+    total_compressed_size_4k: int = 0
+    total_time: int = 0
+    min_ratio: float = float("inf")
+    max_ratio: float = float("-inf")
+    min_ratio4k: float = float("inf")
+    max_ratio4k: float = float("-inf")
+
+def file_scanner(d):
+    if os.path.isfile(d):
+        yield d
+        return
+    for root, dirs, files in os.walk(d, followlinks=False):
+        yield from (os.path.join(root, name) for name in files)
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Compression comparator")
+    parser.add_argument("--methods", default=".*", help="Regular expression of which methods to use.")
+    parser.add_argument("--list", action="store_true", help="List supported compressions methods and exit")
+    parser.add_argument("--progress", action="store_true", help="Precompute size of input and then show progress bar during compression")
+    parser.add_argument("--verbose", action="store_true", help="Show each file being processed and its statistics")
+    parser.add_argument("--debug", action="store_true", help="Show output of executed commands and other details")
+    parser.add_argument("paths", metavar="paths", type=str, nargs='*', help="Files or directories to process recursively")
+    args = parser.parse_args()
+
+    global comps
+
+    if args.list:
+        for comp in comps:
+            print(comp[0])
+        return
+
+    if not args.paths:
+        parser.print_help()
         print()
+        print("Need at least one 'paths' positional argument or run with --list", file=sys.stderr)
+        sys.exit(1)
+
+
+    methods_re = re.compile(args.methods)
+
+    comps = [comp for comp in comps if methods_re.match(comp[0])]
+    if not comps:
+        print("No compression method matched", file=sys.stderr)
+        sys.exit(1)
+
+    comps = [comp for comp in comps if find_exeuctable(comp[2][0]) is not None]
+    if not comps:
+        print("No compression method matched or executables not found", file=sys.stderr)
+        sys.exit(1)
+
+    print("Methods:", " ".join(comp[0] for comp in comps))
+
+    global verbose, debug
+    verbose = args.verbose
+    debug = args.debug
+
+    all_stats = collections.defaultdict(list)
+
+    stats = collections.defaultdict(Stats)
+
+    total_count = 0
+    total_bytes = 0
+    if args.progress:
+        for file_or_dir in args.paths:
+            input_list = file_scanner(file_or_dir)
+            sub_total_count = 0
+            sub_total_bytes = 0
+            for filename in input_list:
+                sub_total_count += 1
+                sub_total_bytes += os.path.getsize(filename)
+            print(file_or_dir, sub_total_count, "files", sub_total_bytes, "bytes")
+            total_count += sub_total_count
+            total_bytes += sub_total_bytes
+
+        import tqdm
+        progress_bar = tqdm.tqdm(total=total_bytes, unit='B', unit_scale=True, unit_divisor=1000, miniters=1)
+    else:
+        progress_bar = None
+
+    next_progress_bytes = 0
+    last_reported_progress_value = -1
+    previous_progress_bytes = 0
+
+    # input_list = sys.argv[1:]
+    for file_or_dir in args.paths:
+        input_list = file_scanner(file_or_dir)
+        for key, stat in process_iterable(input_list):
+            # all_stats[key].append(stat)
+            s = stats[key]
+            s.total_files_count += 1
+            s.total_input_size += stat[1]
+            if args.progress:
+                if s.total_input_size > previous_progress_bytes:
+                     progress_bar.update(s.total_input_size - previous_progress_bytes)
+                     previous_progress_bytes = s.total_input_size
+                #     x = f"{s.total_input_size / total_bytes * 100.0:.0f}%"
+                #     if x != last_reported_progress_value:
+                #         print(x, file=sys.stderr)
+                #         last_reported_progress_value = x
+            s.total_input_size_4k += roundup(stat[1])
+            s.total_compressed_size += stat[2]
+            s.total_compressed_size_4k += roundup(stat[2])
+            s.total_time += stat[0]
+            if stat[1]:
+                ratio = stat[2] / stat[1]
+                s.min_ratio = min(s.min_ratio, ratio)
+                s.max_ratio = max(s.max_ratio, ratio)
+                ratio4k = roundup(stat[2]) / roundup(stat[1])
+                s.min_ratio4k = min(s.min_ratio4k, ratio4k)
+                s.max_ratio4k = max(s.max_ratio4k, ratio4k)
+    if progress_bar:
+        progress_bar.close()
+
+
+    longest_key = max(len(key) for key in stats.keys())
+
+    tab = []
+    for key, s in stats.items():
+        tab.append([
+            key,
+            s.total_files_count,
+            s.total_input_size,
+            s.total_compressed_size, f"{s.total_compressed_size / s.total_input_size * 100.0:.2f}%",
+            f"{s.min_ratio * 100.0:.1f} - {s.max_ratio * 100.0:.1f}%",
+            s.total_input_size_4k,
+            s.total_compressed_size_4k, f"{s.total_compressed_size_4k / s.total_input_size_4k * 100.0:.2f}%",
+            f"{s.min_ratio4k * 100.0:.1f} - {s.max_ratio4k * 100.0:.1f}%",
+            f"{s.total_input_size / s.total_time / 1024 / 1024:2f} MiB/s",
+        ])
+
+    headers =  ["comp", "count", "in_size", "out_size", "ratio", "min-max", "in_size4k", "out_size4k", "ratio", "min-max", "speed"]
+    colalign = ["left", "right", "right",   "right",    "right", "right",   "right",     "right",      "right", "right",   "right"]
+
+    try:
+        import tabulate
+        print(tabulate.tabulate(tab, headers=headers, colalign=colalign, tablefmt="pipe"))
+        print()
+    except Exception:
+        for key, s in stats.items():
+            print(f"{key:{longest_key}s}",
+                  f"count: {s.total_files_count}",
+                  f"input: {s.total_input_size}",
+                  f"input4k: {s.total_input_size_4k}",
+                  f"comp: {s.total_compressed_size} ({s.total_compressed_size / s.total_input_size * 100.0:.2f}%)",
+                  f"comp4k: {s.total_compressed_size_4k} ({s.total_compressed_size_4k / s.total_input_size_4k * 100.0:.2f}%)")
+
+
+    #print()
+    #print("Stats:")
+    #for key, stats in all_stats.items():
+    #    print(f"{key:{longest_key}s}", end="")
+    #    for stat in stats:
+    #        print(f" {stat[2] / max(1, stat[1]):.3f} {stat[0]:8.3f}s {max(1, stat[1]) / max(0.001, stat[0]) / 1e6:.2f}MB/s", end="")
+    #    print()
 
 
 if __name__ == '__main__':
